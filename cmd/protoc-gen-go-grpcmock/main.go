58237fb97bbc21b07257793c10293c5ed16a96fd
solidDoWant <fred.heinecke@yahoo.com>
2025-01-25 10:43:27 +0000

    Set supported editions to track upstream protoc-gen-go supported versions
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- cmd/protoc-gen-go-grpcmock/main.go
+++ cmd/protoc-gen-go-grpcmock/main.go
@@ -19,8 +19,8 @@ import (
 	"flag"
 	"fmt"
 
+	gengo "google.golang.org/protobuf/cmd/protoc-gen-go/internal_gengo"
 	"google.golang.org/protobuf/compiler/protogen"
-	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/lovoo/protoc-gen-go-grpcmock/internal/framework"
@@ -43,8 +43,8 @@ func main() {
 	importPackage := flags.Bool("import_package", false, "Import the file's Go package.")
 	protogen.Options{ParamFunc: flags.Set}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
-		gen.SupportedEditionsMinimum = descriptorpb.Edition_EDITION_PROTO2
-		gen.SupportedEditionsMaximum = descriptorpb.Edition_EDITION_2023
+		gen.SupportedEditionsMinimum = gengo.SupportedEditionsMinimum
+		gen.SupportedEditionsMaximum = gengo.SupportedEditionsMaximum
 		m, err := framework.Mocker(*testFramework)
 		if err != nil {
 			return err
